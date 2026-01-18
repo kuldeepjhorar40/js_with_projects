26b8943047ef0d83d0ea75fd522f22a3ac577779
kudeepjhorar40 <kuldeepjhorar40@gmail.com>
2026-01-18 13:01:14 +0530

    add date

---
+++ .devcontainer/basisjs/Date_and_time.js
@@ -0,0 +1,27 @@
+// date
+
+let date1  = new Date();
+console.log(typeof date1); //object
+
+console.log(date1);//2026-01-17T05:20:39.348Z
+console.log(date1.toString());//Sat Jan 17 2026 10:50:39 GMT+0530 (India Standard Time)
+console.log(date1.toDateString());//Sat Jan 17 2026
+console.log(date1.toISOString());//2026-01-17T08:10:00.002Z
+console.log(date1.toLocaleDateString());//17/1/2026
+console.log(date1.toLocaleString());//17/1/2026, 1:40:00 pm
+console.log(date1.toJSON());//2026-01-17T08:10:00.002Z
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
