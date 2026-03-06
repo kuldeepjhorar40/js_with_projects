169217a8411fc2a9e936e7ea8a2419801d10ce22
kudeepjhorar40 <kuldeepjhorar40@gmail.com>
2026-03-06 18:08:19 +0530

    new js

---
+++ .devcontainer/dom/1domJS.js
@@ -0,0 +1,6 @@
+bd = document.querySelector("body")
+bd.style.backgroundColor  = "red"
+bd.style.color  = "white"
+div1 = document.getElementsByClassName("red")
+div1.setAttribute(color , "blue")
+div1.style.color = "blue"
