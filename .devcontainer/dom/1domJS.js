deaba83ebaed7033abf7086c99a2580f5572c544
kudeepjhorar40 <kuldeepjhorar40@gmail.com>
2026-03-08 23:45:34 +0530

    new dom learning

--- .devcontainer/dom/1domJS.js
+++ .devcontainer/dom/1domJS.js
@@ -2,5 +2,6 @@ bd = document.querySelector("body")
 bd.style.backgroundColor  = "red"
 bd.style.color  = "white"
 div1 = document.getElementsByClassName("red")
-div1.setAttribute(color , "blue")
-div1.style.color = "blue"
+div1[0].style.color = "black"
+div1[0].style.padding= "20px"
+div1[0].style.backgroundColor  = "green"
